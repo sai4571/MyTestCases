470dda1a8ccd44b504f6b56c0bae3cb73005b5e5
sai4571 <110476592+sai4571@users.noreply.github.com>
2022-08-05 18:31:36 +0530

    Update App.java

--- src/main/java/gittest/App.java
+++ src/main/java/gittest/App.java
@@ -1,1 +1,13 @@
+package sai.gittest;
 
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
